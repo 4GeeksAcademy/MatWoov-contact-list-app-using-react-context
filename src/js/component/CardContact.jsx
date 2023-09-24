340441256968fb1915d71e5f417ce37922c4867b
Mathew Woo <133248984+MatWoov@users.noreply.github.com>
2023-09-24 22:20:28 +0000

    wip: edited the form

--- src/js/component/CardContact.jsx
+++ src/js/component/CardContact.jsx
@@ -8,7 +8,6 @@ export const CardContact = (props) => {
 
     const deleteContact = (id) => {
         actions.deleteContact(id);
-        actions.getUsers();
     };
 
 
--- src/js/views/AddContact.jsx
+++ src/js/views/AddContact.jsx
@@ -1,21 +1,48 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext.js";
+import { Link } from "react-router-dom";
 
 
 export const AddContact = () => {
     const { store, actions } = useContext(Context)
 
+    const [form, setForm] = useState({
+        full_name: "",
+        email: "",
+        agenda_slug: "agenda_matwoo",
+        address: "",
+        phone: ""
+    })
+
+    const onChangeName = (event) => {
+        setForm((currentState) => {
+            return { ...currentState, full_name: event.target.value }
+        })
+    };
+
+    const onChangeEmail = (event) => {
+        setForm((currentState) => {
+            return { ...currentState, email: event.target.value }
+        })
+    };
+
+    const onChangeAddress = (event) => {
+        setForm((currentState) => {
+            return { ...currentState, address: event.target.value }
+        })
+    };
+
+    const onChangePhone = (event) => {
+        setForm((currentState) => {
+            return { ...currentState, phone: event.target.value }
+        })
+    };
+
     const addContact = () => {
         console.log('Ejecuto Bton');
-        const user= {
-            full_name: "Pedro",
-            email: "pedro@gmail.com",
-            agenda_slug: "agenda_matwoo",
-            address:"47568 NW 34ST, 33434 FL, Miami",
-            phone:"7864445566"
-        }
+        const user = form;
         actions.addContact(user);
-      }
+    }
 
 
     return (
@@ -24,22 +51,24 @@ export const AddContact = () => {
             <form className="p-5">
                 <div className="mb-3">
                     <label htmlFor="name" className="form-label">Full Name</label>
-                    <input name="name" type="text" className="form-control" placeholder="Full Name" />
+                    <input name="name" type="text" onChange={onChangeName} value={form.full_name} className="form-control" placeholder="Full Name" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email</label>
-                    <input name="email" type="email" className="form-control" aria-describedby="emailHelp" placeholder="Enter Mail" />
+                    <input name="email" type="email" onChange={onChangeEmail} value={form.email} className="form-control" aria-describedby="emailHelp" placeholder="Enter Mail" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="phone" className="form-label">Phone</label>
-                    <input name="phone" type="text" className="form-control" placeholder="Enter Phone" />
+                    <input name="phone" type="text" onChange={onChangePhone} value={form.phone} className="form-control" placeholder="Enter Phone" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="address" className="form-label">Address</label>
-                    <input name="address" type="text" className="form-control" placeholder="Enter Address" />
+                    <input name="address" type="text" onChange={onChangeAddress} value={form.address} className="form-control" placeholder="Enter Address" />
                 </div>
                 <div className="d-grid gap-2">
-                    <button className="btn btn-primary" onClick={addContact} type="button">Save</button>
+                    <Link to="/">
+                        <button className="btn btn-primary" onClick={addContact} type="button">Save</button>
+                    </Link>
                 </div>
             </form>
         </div>
